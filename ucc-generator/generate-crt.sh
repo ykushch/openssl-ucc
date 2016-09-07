fb1181fbff44db82abd92062dfa5b687dc5e289a
Yuri <yuri.kushch@gmail.com>
2016-09-07 12:38:24 +0300

    Fix shellcheck issues

--- ucc-generator/generate-crt.sh
+++ ucc-generator/generate-crt.sh
@@ -7,8 +7,8 @@ createCrtLocally() {
     openSslName="$2"
     keyName="server"
     crtName="server"
-    openssl req -x509 -nodes -days ${number_of_days} -newkey rsa:2048 -keyout ${keyName}.key -extensions v3_req -out ${crtName}.crt -config ${openSslName}.cnf
-    printf "\033[0;35mCertificate ${crtName} is ready ${normalTextFormat}\n"
+    openssl req -x509 -nodes -days "${number_of_days}" -newkey rsa:2048 -keyout "${keyName}".key -extensions v3_req -out "${crtName}".crt -config "${openSslName}".cnf
+    printf "\033[0;35mCertificate %s is ready %s\n" "${crtName}" "${normalTextFormat}"
 }
 
 showAvailableCommands() {
@@ -19,7 +19,7 @@ showAvailableCommands() {
 # Default parameters
 
 # Parse arguments
-while [[ $# > 0 ]]
+while [[ $# -gt 0 ]]
 do
 key="$1"
 
@@ -42,20 +42,19 @@ done
 # Number of days for certificate
 if [ -z "${NUMBER_OF_DAYS}" ]; then
     NUMBER_OF_DAYS=365
-    printf "Using DEFAULT NUMBER of valid days for certificate as ${NUMBER_OF_DAYS}\n"
+    printf "Using DEFAULT NUMBER of valid days for certificate as %s\n" "${NUMBER_OF_DAYS}"
 fi
 
 # Path to the SSL certificate
 if [ -z "${CONFIG_OPENSSL_NAME}" ]; then
     CONFIG_OPENSSL_NAME="openssl.modified"
-    printf "Using DEFAULT NAME for OpenSSL certificate name as ${CONFIG_OPENSSL_NAME}\n"
+    printf "Using DEFAULT NAME for OpenSSL certificate name as %s\n" "${CONFIG_OPENSSL_NAME}"
 fi
 
 if [ ! -f ./${CONFIG_OPENSSL_NAME}.cnf ]; then
-    printf "File ${CONFIG_OPENSSL_NAME}.cnf cannot be found locally\n"
+    printf "File %s.cnf cannot be found locally\n" "${CONFIG_OPENSSL_NAME}"
     printf "Exiting...\n"
     exit 1
 fi
 
 createCrtLocally ${NUMBER_OF_DAYS} ${CONFIG_OPENSSL_NAME}
-
